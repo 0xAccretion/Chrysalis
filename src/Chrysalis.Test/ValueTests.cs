dc17f17b482d2e02a4b26aa6daed34cb6a27f7a5
0xAccretion <xaccretion@outlook.com>
2023-09-27 19:11:30 +0800

    Improved Cardano Value Type and Tests

--- src/Chrysalis.Test/ValueTests.cs
+++ src/Chrysalis.Test/ValueTests.cs
@@ -47,4 +47,61 @@ public class ValueTests
         // Then
         Assert.True(originalCborData.SequenceEqual(serializedCborData));
     }
+
+    [Fact]
+    public void TestCoinValueFromToHex()
+    {
+        // Given
+        string originalHex = "1a001629b6";  // Hex string representation of the original CBOR data
+        CoinValue? originalCoinValue = ByteConvertibleFactory.FromHex<CoinValue>(originalHex);
+
+        // When
+        string convertedHex = originalCoinValue?.ToHexString()!;
+
+        // Then
+        Assert.Equal(originalHex.ToLowerInvariant(), convertedHex.ToLowerInvariant());
+    }
+
+    [Fact]
+    public void TestCoinValueFromToBytes()
+    {
+        // Given
+        byte[] originalBytes = Convert.FromHexString("1a001629b6");  // Original CBOR data
+        CoinValue? originalCoinValue = ByteConvertibleFactory.FromBytes<CoinValue>(originalBytes);
+
+        // When
+        byte[]? convertedBytes = originalCoinValue?.ToByteArray();
+
+        // Then
+        Assert.True(originalBytes.SequenceEqual(convertedBytes!));
+    }
+
+    // Similar tests for MultiAssetValue
+    [Fact]
+    public void TestMultiAssetValueFromToHex()
+    {
+        // Given
+        string originalHex = "821A001629B6A1581C6F37A98BD0C9CED4E302EC2FB3A2F19FFBA1B5C0C2BEDEE3DAC30E56A45148595045534B554C4C535F56545F505F45015148595045534B554C4C535F56545F565F43015248595045534B554C4C535F56545F4D5F4545015348595045534B554C4C535F56545F41435F454501";
+        MultiAssetValue? originalMultiAssetValue = ByteConvertibleFactory.FromHex<MultiAssetValue>(originalHex);
+
+        // When
+        string convertedHex = originalMultiAssetValue?.ToHexString()!;
+
+        // Then
+        Assert.Equal(originalHex.ToLowerInvariant(), convertedHex.ToLowerInvariant());
+    }
+
+    [Fact]
+    public void TestMultiAssetValueFromToBytes()
+    {
+        // Given
+        byte[] originalBytes = Convert.FromHexString("821A001629B6A1581C6F37A98BD0C9CED4E302EC2FB3A2F19FFBA1B5C0C2BEDEE3DAC30E56A45148595045534B554C4C535F56545F505F45015148595045534B554C4C535F56545F565F43015248595045534B554C4C535F56545F4D5F4545015348595045534B554C4C535F56545F41435F454501");
+        MultiAssetValue? originalMultiAssetValue = ByteConvertibleFactory.FromBytes<MultiAssetValue>(originalBytes);
+
+        // When
+        byte[]? convertedBytes = originalMultiAssetValue?.ToByteArray();
+
+        // Then
+        Assert.True(originalBytes.SequenceEqual(convertedBytes!));
+    }
 }
--- src/Chrysalis/Cardano/Models/Value.cs
+++ src/Chrysalis/Cardano/Models/Value.cs
@@ -15,26 +15,65 @@ public interface IValue
     ulong Coin { get; set; }
 }
 
+/// <summary>
+/// Represents a value in Cardano that consists of a single coin.
+/// </summary>
 public class CoinValue : ByteConvertibleBase, ICborObject, IValue
 {
+    /// <summary>
+    /// The amount of the base currency (coin) in the value.
+    /// </summary>
     public ulong Coin { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CoinValue"/> class with a coin value of 0.
+    /// </summary>
     public CoinValue() : base(Array.Empty<byte>())
     {
         Coin = 0;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CoinValue"/> class with the specified coin value.
+    /// </summary>
+    /// <param name="coin">The amount of the base currency (coin) in the value.</param>
     public CoinValue(ulong coin) : base(Array.Empty<byte>())
     {
         Coin = coin;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CoinValue"/> class with the specified hex string.
+    /// </summary>
+    /// <param name="hex">The hex string to initialize the instance with.</param>
+    public CoinValue(string hex) : base(Array.Empty<byte>())
+    {
+        FromCbor(Convert.FromHexString(hex));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CoinValue"/> class with the specified CBOR data.
+    /// </summary>
+    /// <param name="cborData">The CBOR data to initialize the instance with.</param>
+    public CoinValue(byte[] cborData) : base(Array.Empty<byte>())
+    {
+        FromCbor(cborData);
+    }
+
+    /// <summary>
+    /// Deserializes the instance from the specified CBOR data.
+    /// </summary>
+    /// <param name="data">The CBOR data to deserialize the instance from.</param>
     public void FromCbor(byte[] data)
     {
         var reader = new CborReader(data);
         FromCbor(reader);
     }
 
+    /// <summary>
+    /// Serializes the instance to CBOR data.
+    /// </summary>
+    /// <returns>The serialized CBOR data.</returns>
     public byte[] ToCbor()
     {
         var writer = new CborWriter(CborConformanceMode.Strict);
@@ -42,35 +81,98 @@ public class CoinValue : ByteConvertibleBase, ICborObject, IValue
         return writer.Encode();
     }
 
+    /// <summary>
+    /// Deserializes the instance from the specified CBOR reader.
+    /// </summary>
+    /// <param name="reader">The CBOR reader to deserialize the instance from.</param>
     public void FromCbor(CborReader reader)
     {
         Coin = reader.ReadUInt64();
     }
-}
-
-public class MultiAssetValue : ByteConvertibleBase, ICborObject, IValue
-{
-    public ulong Coin { get; set; }
-    public MultiAsset Assets { get; set; }
-
-    public MultiAssetValue() : base(Array.Empty<byte>())
+    
+    /// <summary>
+    /// Serializes the instance to a byte array.
+    /// </summary>
+    /// <returns>The serialized byte array.</returns>
+    public override byte[] ToByteArray()
     {
-        Coin = 0;
-        Assets = new MultiAsset(); // Initialize with a default constructor or however you prefer
+        return ToCbor();
     }
 
+    /// <summary>
+    /// Serializes the instance to a hex string.
+    /// </summary>
+    /// <returns>The serialized hex string.</returns>
+    public override string ToHexString()
+    {
+        return Convert.ToHexString(ToCbor()).ToLowerInvariant();
+    }
+}
+
+/// <summary>
+/// Represents a value in Cardano that consists of a combination of coins and other assets.
+/// </summary>
+public class MultiAssetValue : ByteConvertibleBase, ICborObject, IValue
+{
+    /// <summary>
+    /// The amount of the base currency (coin) in the value.
+    /// </summary>
+    public ulong Coin { get; set; } = 0;
+
+    /// <summary>
+    /// The assets included in the value.
+    /// </summary>
+    public MultiAsset Assets { get; set; } = new MultiAsset();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultiAssetValue"/> class with a coin value of 0 and no assets.
+    /// </summary>
+    public MultiAssetValue() : base(Array.Empty<byte>())
+    { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultiAssetValue"/> class with the specified coin value and assets.
+    /// </summary>
+    /// <param name="coin">The amount of the base currency (coin) in the value.</param>
+    /// <param name="assets">The assets included in the value.</param>
     public MultiAssetValue(ulong coin, MultiAsset assets) : base(Array.Empty<byte>())
     {
         Coin = coin;
         Assets = assets;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultiAssetValue"/> class with the specified hex string.
+    /// </summary>
+    /// <param name="hex">The hex string to initialize the instance with.</param>
+    public MultiAssetValue(string hex) : base(Array.Empty<byte>())
+    {
+        FromCbor(Convert.FromHexString(hex));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultiAssetValue"/> class with the specified CBOR data.
+    /// </summary>
+    /// <param name="cborData">The CBOR data to initialize the instance with.</param>
+    public MultiAssetValue(byte[] cborData) : base(Array.Empty<byte>())
+    {
+        FromCbor(cborData);
+    }
+
+    /// <summary>
+    /// Deserializes the instance from the specified CBOR data.
+    /// </summary>
+    /// <param name="data">The CBOR data to deserialize the instance from.</param>
     public void FromCbor(byte[] data)
     {
         var reader = new CborReader(data);
         FromCbor(reader);
     }
 
+    /// <summary>
+    /// Deserializes the instance from the specified CBOR reader.
+    /// </summary>
+    /// <param name="reader">The CBOR reader to deserialize the instance from.</param>
     public void FromCbor(CborReader reader)
     {
         reader.ReadStartArray();
@@ -80,6 +182,10 @@ public class MultiAssetValue : ByteConvertibleBase, ICborObject, IValue
         reader.ReadEndArray();
     }
 
+    /// <summary>
+    /// Serializes the instance to CBOR data.
+    /// </summary>
+    /// <returns>The serialized CBOR data.</returns>
     public byte[] ToCbor()
     {
         var writer = new CborWriter(CborConformanceMode.Strict);
@@ -88,5 +194,23 @@ public class MultiAssetValue : ByteConvertibleBase, ICborObject, IValue
         writer.WriteEncodedValue(Assets.ToCbor());
         writer.WriteEndArray();
         return writer.Encode();
+    }
+
+    /// <summary>
+    /// Serializes the instance to a byte array.
+    /// </summary>
+    /// <returns>The serialized byte array.</returns>
+    public override byte[] ToByteArray()
+    {
+        return ToCbor();
+    }
+
+    /// <summary>
+    /// Serializes the instance to a hex string.
+    /// </summary>
+    /// <returns>The serialized hex string.</returns>
+    public override string ToHexString()
+    {
+        return Convert.ToHexString(ToCbor()).ToLowerInvariant();
     }
 }
